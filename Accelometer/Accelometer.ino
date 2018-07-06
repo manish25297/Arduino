27c16fc33bdad200398bf3d8135a98027a40df34
manish kumar singh <manish.25297@gmail.com>
2018-07-06 20:48:39 +0530

    Add files via upload

---
+++ Accelometer/Accelometer.ino
@@ -0,0 +1,19 @@
+const int xpin = A3;
+const int ypin = A2;
+const int zpin = A1;
+void setup() {
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+}
+
+void loop() {
+  Serial.print(analogRead(xpin));
+  Serial.print("\t");
+  Serial.print(analogRead(ypin));
+  Serial.print("\t");
+  Serial.print(analogRead(zpin));
+  Serial.println();
+  delay(1000);
+
+
+}
---
+++ print_lcd_using_BT/print_lcd_using_BT.ino
@@ -0,0 +1,23 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
+String st;
+
+void setup() {
+  // put your setup code here, to run once:
+lcd.begin(16,2);
+Serial.begin(9600);
+}
+
+void loop() {
+ if(Serial.available()>0)
+ {
+
+  lcd.setCursor(0,0);
+ lcd.clear();
+ st=Serial.readString();
+lcd.print(st);
+Serial.println(st);
+
+  
+}
+}
