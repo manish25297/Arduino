2d8763161e45ace3bfdff25887558e81a8db97b5
manish kumar singh <manish.25297@gmail.com>
2018-07-06 20:22:56 +0530

    Add files via upload

---
+++ bmp180 pressure sensor/sketch_jul12b/sketch_jul12b.ino
@@ -0,0 +1,58 @@
+#include <Adafruit_BMP085.h>
+#include <Wire.h>
+//#include <LiquidCrystal.h>
+// initialize the library with the numbers of the interface pins
+//LiquidCrystal lcd(8, 9, 10, 11, 12, 13);//RS,EN,D4,D5,D6,D7
+ 
+char PRESSURESHOW[4];// initializing a character of size 4 for showing the  result
+char TEMPARATURESHOW[4];// initializing a character of size 4 for showing the temparature result
+ 
+Adafruit_BMP085 bmp;
+ 
+void setup() {
+                /*lcd.begin(16, 2);
+                // Print a logo message to the LCD.
+                lcd.print("  CIRCUITDIGEST");
+                lcd.setCursor(0, 1);*/
+                delay (2500);
+                delay (2500);
+               
+                //lcd.clear();//clear display
+ 
+                // Print another message to the LCd
+ 
+                Serial.begin(9600);
+                if (!bmp.begin())
+                {
+                                Serial.println("ERROR");///if there is an error in communication
+                                while (1) {}
+                }
+}
+ 
+void loop()
+{
+ 
+               
+                Serial.println("Pressure= ");          // print name
+               
+                String PRESSUREVALUE = String(bmp.readPressure());
+                // convert the reading to a char array
+                PRESSUREVALUE.toCharArray(PRESSURESHOW, 4);
+                Serial.println(PRESSURESHOW);
+                Serial.print("hPa   ");
+               // lcd.setCursor(0, 1);
+                //// set the cursor to column 0, line 1
+ 
+                Serial.print("Tempar.=");// print name
+ 
+               
+                String TEMPARATUREVALUE = String(bmp.readTemperature());
+                // convert the reading to a char array
+                TEMPARATUREVALUE.toCharArray(TEMPARATURESHOW, 4);
+                Serial.println(TEMPARATURESHOW);
+                Serial.print("C   ");
+               
+               // lcd.setCursor(0, 0);/ set the cursor to column 0, line1
+ 
+                delay(1000);
+}
---
+++ multiple_seven_segment/multiple_seven_segment.ino
@@ -0,0 +1,37 @@
+int  DATA=6;
+int  LATCH=8;
+int  CLOCK=10;
+int digits[] = {252, 96, 218, 242, 102, 182, 190, 224, 254, 246, 238, 62, 156, 
+122, 158, 142};
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LATCH, OUTPUT);
+   pinMode(CLOCK, OUTPUT);
+   pinMode(DATA, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int i;
+for( int n=0;n<100;n++)
+{ int left, right=0;
+  if (n < 10)
+  {
+    digitalWrite(LATCH, LOW);
+    shiftOut(DATA, CLOCK, LSBFIRST, digits[n]);
+    shiftOut(DATA, CLOCK, LSBFIRST,0);  
+    digitalWrite(LATCH, HIGH);
+  }
+  else if (n >= 10)
+  {     right = n % 10; // remainder of dividing the number to display by 10
+    left = n / 10;  // quotient of dividing the number to display by 10
+    digitalWrite(LATCH, LOW);
+    shiftOut(DATA, CLOCK, LSBFIRST, digits[right]);
+    shiftOut(DATA, CLOCK, LSBFIRST, digits[left]);    
+    digitalWrite(LATCH, HIGH);
+  }
+  delay(500);
+}
+}
